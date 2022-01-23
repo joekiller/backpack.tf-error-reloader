896fcb4d61b177f74c77d81084537882da4df486
Joseph Lawson <joe@joekiller.com>
2022-01-23 17:23:21 -0500

    better include statements

--- backpack.tf error reloader.user.js
+++ backpack.tf error reloader.user.js
@@ -1,13 +1,14 @@
 // ==UserScript==
 // @name         backpack.tf - Page Reloader
 // @description  Recovers from various backpack.tf error responses
-// @version      0.1.0
+// @version      0.1.1
 // @author       joekiller
 // @namespace    https://github.com/joekiller
-// @include      /^https?:\/\/backpack\.tf\/.*
+// @include     /^https?:\/\/(.*\.)?backpack\.tf(:\d+)?\/item\/\d+/
+// @include     /^https?:\/\/(.*\.)?backpack\.tf(:\d+)?\/premium\/search.*/
+// @include     /^https?:\/\/(.*\.)?backpack\.tf\/profiles\/\d{17}/
 // @downloadURL  https://github.com/joekiller/backpack.tf-error-reloader/raw/master/backpack.tf%20error%20reloader.user.js
 // @updateURL    https://github.com/joekiller/backpack.tf-error-reloader/raw/master/backpack.tf%20error%20reloader.meta.js
-// @include      /^https?:\/\/backpack\.tf\/.*
 // @grant        none
 // ==/UserScript==
 
