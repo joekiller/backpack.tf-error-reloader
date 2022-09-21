14d177c677559ad2c1e37a41fee63065a5fdefc5
Joseph Lawson <joe@joekiller.com>
2022-09-21 09:15:37 -0400

    0.2.0

--- backpack.tf error reloader.meta.js
+++ backpack.tf error reloader.meta.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         backpack.tf - Page Reloader
 // @description  Recovers from various backpack.tf error responses
-// @version      0.1.2
+// @version      0.2.0
 // @author       joekiller
 // @namespace    https://github.com/joekiller
 // @include      /^https?:\/\/(.*\.)?backpack\.tf\/.*
